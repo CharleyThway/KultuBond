bc6a2aba230d2529ac49767b6d4fa4b655c9db46
Chit-Su-Thway <chitsuthway40@gmail.com>
2024-09-30 01:42:14 +0900

    Sign in page

--- app/index.jsx
+++ app/index.jsx
@@ -6,6 +6,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import PrimaryButton from "../components/PrimaryButton";
 import SecondaryButton from "../components/SecondaryButton";
+// import Loader from "../components";
 //import { useGlobalContext } from "../context/GlobalProvider"; // Assuming you have a global context setup
 
 const Welcome = () => {
@@ -16,27 +17,39 @@ const Welcome = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       {/* Background Image */}
-      <Image
-        source={images.mainBackground} // Update the image path based on your structure
-        className="absolute w-full h-full"
-        resizeMode="cover"
-      />
       <ScrollView
         contentContainerStyle={{
           height: "100%",
         }}
       >
+        <Image
+          source={images.mainBackground} // Update the image path based on your structure
+          className="absolute w-full h-full"
+          resizeMode="cover"
+        />
         {/* Overlay */}
         <View className="absolute w-full h-full bg-black/50" />
 
         {/* Main Content */}
         <View className="flex flex-col justify-center items-center h-full">
           {/* Title */}
-          <Text className="text-5xl font-bold text-white mb-4 tracking-widest">
-            KULTU BOND
-          </Text>
+          <View className="w-full px-10 mt-10 mb-20">
+            {/* Title Container with Flexbox */}
+            <View className="flex flex-col">
+              {/* "Kultu" aligned to the left */}
+              <Text className="text-6xl font-bold text-white tracking-widest">
+                KULTU
+              </Text>
+
+              {/* "Bond" aligned to the right and positioned below "Kultu" */}
+              <Text className="text-6xl font-bold text-white tracking-widest text-right">
+                BOND
+              </Text>
+            </View>
+          </View>
 
           {/* Subtitle */}
+
           <Text className="text-center text-white text-base mb-10 px-6">
             Embark on an unforgettable journey through Korean culture along with
             fellow participants of World Youth Day 2027
@@ -48,12 +61,13 @@ const Welcome = () => {
             <PrimaryButton
               title="Join us"
               handlePress={() => router.push("/sign-up")}
+              containerStyles="mr-4"
             />
 
             {/* Sign-in button */}
             <SecondaryButton
               title="Sign in"
-              handlePress={() => router.push("/sign-up")}
+              handlePress={() => router.push("/sign-in")}
             />
           </View>
         </View>
--- constants/icons.js
+++ constants/icons.js
@@ -27,3 +27,8 @@
 //   eyeHide,
 //   eye,
 // };
+
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+
+export default { eyeHide, eye };
